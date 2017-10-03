40f526821ba096fbc4129a701b0d8aa8754ce0f0
Anand Vijay Singh <Anand Vijay Singh>
2017-10-03 16:52:55 +0530

    first commit for github deployment from /docs folder of project.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  private navLinks: any[];
+  navLinks: any[];
 
   constructor() {
     console.log('app-constructed');
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  private title = 'Angular 2 with p5 js.';
+  title = 'Angular 2 with p5 js.';
   constructor() {
     console.log('home-constructed');
   }
