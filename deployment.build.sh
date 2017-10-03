7997fdde2859309588e3ce8fe2698e2629c32302
Anand Vijay Singh <Anand Vijay Singh>
2017-10-03 16:57:44 +0530

    deployment.build.sh script for github deployment.

---
+++ deployment.build.sh
@@ -0,0 +1,13 @@
+#!/usr/sh
+# https://github.com/angular/angular-cli/wiki/stories-github-pages
+# ng build --prod --output-path docs --base-href PROJECT_NAME
+ng build --prod --output-path docs --base-href angular4-p5js-app
+
+git add --all
+
+git commit -m 'commit message'
+
+git push -u origin master
+
+
+
